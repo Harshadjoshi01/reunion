ac0438ae1418486ee68f2d37267abb89a84c61e8
HarshadRoundtech <harshadj@roundtechsquare.com>
2023-04-13 13:01:04 +0530

    validators added

--- Routes/posts.router.js
+++ Routes/posts.router.js
@@ -10,13 +10,15 @@ const {
   getallposts,
 } = require("../controllers/posts.controller");
 const { verifyAccessToken } = require("../validators/jwtvalidation");
+const {PostSchema, CommentSchema, ParamsSchema} = require("../helpers/ValidationSchema");
+const {JoiValidator} = require('../validators/joivalidator');
 
-router.post("/posts", verifyAccessToken, createpost);
-router.delete("/posts/:id", verifyAccessToken, deletepost);
-router.post("/like/:id", verifyAccessToken, likepost);
-router.post("/unlike/:id", verifyAccessToken, unlikepost);
-router.post("/comment/:id", verifyAccessToken, addcomment);
-router.get("/posts/:id", verifyAccessToken, getpost);
+router.post("/posts", verifyAccessToken, JoiValidator(PostSchema), createpost);
+router.delete("/posts/:id", verifyAccessToken, JoiValidator(ParamsSchema), deletepost);
+router.post("/like/:id", verifyAccessToken,JoiValidator(ParamsSchema), likepost);
+router.post("/unlike/:id", verifyAccessToken,JoiValidator(ParamsSchema), unlikepost);
+router.post("/comment/:id", verifyAccessToken, JoiValidator(CommentSchema), JoiValidator(ParamsSchema),addcomment);
+router.get("/posts/:id", verifyAccessToken,JoiValidator(ParamsSchema), getpost);
 router.get("/all_posts", verifyAccessToken, getallposts);
 
 module.exports = router;
--- helpers/ValidationSchema.js
+++ helpers/ValidationSchema.js
@@ -10,8 +10,28 @@ const LoginSchema = Joi.object({
     email: Joi.string().min(6).max(255).lowercase().required().email(),
     password: Joi.string().min(6).max(1024).required(),
   });
+
+  const PostSchema = Joi.object({
+    title: Joi.string().min(6).max(255).required(),
+    description: Joi.string().min(6).max(1024).required(),
+  });
+
+  const CommentSchema = Joi.object({
+    comment: Joi.string().min(6).max(1024).required(),
+  });
+
+  // validation schema for path params id
+  const ParamsSchema = Joi.object({
+    params: Joi.object({
+      id: Joi.string().required(),
+    }),
+  });
+
 module.exports = {
     LoginSchema,
     RegisterSchema,
+    PostSchema,
+    CommentSchema,
+    ParamsSchema,
     };
     
